143b274c6fce8dbe678a9ff99351c1b760c521ce
bdol <bdolmail@gmail.com>
2014-11-07 17:13:41 -0800

    Added ability to write features

--- data_utils/dataset.py
+++ data_utils/dataset.py
@@ -1,4 +1,4 @@
-from feature_creator import parse_gid
+from feature_creator import parse_gid, feat_score_diff
 import ConfigParser
 import sys
 
@@ -136,13 +136,26 @@ class Dataset:
                 home_d = self.raw_and_avg_features[vals["home_team_id"]][vals[
                     "year"]][vals["week"]]
 
+                this_week = str(int(vals["week"])+1)
+                away_score = self.raw_and_avg_features[vals["away_team_id"]][
+                    vals["year"]][this_week]["ScoreOff"]
+                home_score = self.raw_and_avg_features[vals["home_team_id"]][vals[
+                    "year"]][this_week]["ScoreOff"]
+
+                self.features[gid]["t_score_diff"] = float(home_score) - float(
+                    away_score)
+                self.features[gid]["t_home_team_won"] = "1" if float(
+                    home_score) > float(away_score) else "0"
+
                 for f in inc_feats:
                     if f == "c_away_team_id":
                         self.features[gid][f] = vals["away_team_id"]
                     if f == "c_home_team_id":
                         self.features[gid][f] = vals["home_team_id"]
+
                     if f == "n_game_week":
                         self.features[gid][f] = str(int(vals["week"]) + 1)
+
                     if f == "n_off_score_diff":
                         self.features[gid][f] = float(home_d["avg_ScoreOff"])\
                                                 - float(away_d["avg_ScoreOff"])
@@ -163,6 +176,7 @@ class Dataset:
                     if f == "n_def_pass_yds_diff":
                         self.features[gid][f] = float(home_d[
                             "avg_PassYdsDef"]) - float(away_d["avg_PassYdsDef"])
+
 
     def create_dataset(self, conf_filename, team_ids, gids, raw_data):
         self.data = dict((k, None) for k in gids)
@@ -202,9 +216,37 @@ class Dataset:
         self.compute_average_feature_vals(raw_data)
         self.compute_features(cfg.options("features"), gids)
 
+        self.write_dataset("../data/datasets/trainupto2013_test2013/train.csv")
 
-    def write_dataset(filename, dataset):
-        pass
 
-    def read_dataset(filename):
+    def write_dataset(self, filename):
+        f = open(filename, "w")
+
+        headers_written = False
+        for k in sorted(self.features):
+            headers = sorted(self.features[k])
+            headers.remove("t_score_diff")
+            headers.remove("t_home_team_won")
+
+            if not headers_written:
+                f.write("gid")
+                f.write("\tt_score_diff")
+                f.write("\thome_team_won")
+                for h in headers:
+                    f.write("\t{0}".format(h))
+                f.write("\n")
+                headers_written = True
+
+            f.write("{0}".format(k))
+            f.write("\t{0}".format(self.features[k]["t_score_diff"]))
+            f.write("\t{0}".format(self.features[k]["t_home_team_won"]))
+            for h in headers:
+                f.write("\t{0}".format(self.features[k][h]))
+
+            f.write("\n")
+
+
+        f.close()
+
+    def read_dataset(self, filename):
         pass
